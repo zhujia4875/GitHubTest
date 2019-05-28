c9878f33aa9721597a6893ed2ece7caff647122d
zhuzhengjia_dh <zhuzhengjia_dh@bonc.com.cn>
2019-05-28 09:37:52 +0800

    TEST

---
+++ mmmm/src/com/aa/test/Test.java
@@ -0,0 +1,12 @@
+package com.aa.test;
+
+/**
+ * @author zhuzhengjia
+ * @create 2019-05-28 9:35
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+    }
+
+}
